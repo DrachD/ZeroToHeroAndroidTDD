479eac928a4da17cf6fb31ace1f1ebcc025c29cb
Dmitriy <drachdmitriy21@gmail.com>
2024-07-23 16:23:38 +0300

    task completed

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/App.kt
@@ -0,0 +1,15 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.app.Application
+
+class App : Application() {
+
+    lateinit var viewModel: MainViewModel
+
+    override fun onCreate() {
+        super.onCreate()
+        val liveDataWrapper = LiveDataWrapper.Base()
+        val repository = Repository.Base()
+        viewModel = MainViewModel(liveDataWrapper, repository)
+    }
+}
